15b6c37416cc8fb66bd148de1407c4f543a4f2b2
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2023-09-25 10:01:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import json
 # import pickle
 # import streamlit as st
 # from streamlit_lottie import st_lottie
@@ -16,22 +15,20 @@ import plotly.graph_objects as go
 import cufflinks as cf
 
 
-
 def make_clickable(get_list):
     link, title = get_list[0], get_list[1]
     # text = link.split('=')[1]
     return f'<a target="aboutlink" href="{link}">{title}</a>'
 
+
 # 페이지 설정
 st.set_page_config(
-    layout="wide",  
-    initial_sidebar_state="collapsed"  
+    layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-
 
 ################# Style option
 left_mg = 0
@@ -48,14 +45,9 @@ hide_table_row_index = """
             """
 
 ################# DATA LOAD
-df = pd.read_excel('./resource/08_2023-09-21_complete.xlsx')   # 06_2023-09-15_complete.xlsx
+df = pd.read_excel('./resource/08_2023-09-21_complete.xlsx')  # 06_2023-09-15_complete.xlsx
 df['날짜'] = (pd.to_datetime(df['날짜']).dt.strftime('%Y-%m-%d'))
 df['날짜'] = df['날짜'].astype(str)
-
-
-
-
-
 
 # 사이드바에 내용 추가
 st.sidebar.header("Side Bar")
@@ -65,7 +57,7 @@ white_space_1, data_space, white_space_2 = st.columns([0.1, 0.8, 0.1])
 with white_space_1:
     st.empty()
 
-with data_space :
+with data_space:
     ######################### HEAD
     # 메인 콘텐츠 영역에 내용 추가
     st.title(":newspaper:퇴직연금 동향 뉴스")
@@ -73,35 +65,33 @@ with data_space :
     ''
     ''
     ######################### 차트 화면
-    st.subheader('뉴스 분석', help="수집한 결과를 차트로 파악하기 쉽게 도식화 해두었습니다.\n- 주별뉴스현황 : 카테고리별로 기사 발행 현황에 대해 막대차트로 확인가능합니다.\n- 기업관련 기사 조회 : 기업별로 발행한 퇴직연금 관련 기사 수를 파이차트로 비중을 도식화 하였습니다. ")
+    st.subheader('뉴스 분석',
+                 help="수집한 결과를 차트로 파악하기 쉽게 도식화 해두었습니다.\n- 주별뉴스현황 : 카테고리별로 기사 발행 현황에 대해 막대차트로 확인가능합니다.\n- 기업관련 기사 조회 : 기업별로 발행한 퇴직연금 관련 기사 수를 파이차트로 비중을 도식화 하였습니다. ")
     subject_chart, company_chart = st.columns(2, gap="large")
-
 
     ##### CHAT01. 뉴스 주제별 bar chat
     with subject_chart:
-
         st.markdown(
-            '<div style = "color:white; font-size: 16px; text-align:center; background-color: #2d3d4a">주별 뉴스 현황</div>', # #403466  2d3d4a
+            '<div style = "color:white; font-size: 16px; text-align:center; background-color: #2d3d4a">주별 뉴스 현황</div>',
+            # #403466  2d3d4a
             unsafe_allow_html=True)
         st.markdown('<h3 style="text-align:center">   </h3>', unsafe_allow_html=True)
         df_pivot = pd.pivot_table(df, index='날짜',
                                   columns='분류',
                                   values='제목', aggfunc='count').fillna(0)
 
-        fig_amt = df_pivot.iplot(kind='bar', barmode='stack', asFigure=True, dimensions=(400,400),
-                                 colors=('#7f999f','#a2a9cd',  '#77adda', '#85d3e6', '#0eccfb', '#2ebbc9'))
+        fig_amt = df_pivot.iplot(kind='bar', barmode='stack', asFigure=True, dimensions=(400, 400),
+                                 colors=('#7f999f', '#a2a9cd', '#77adda', '#85d3e6', '#0eccfb', '#2ebbc9'))
 
         fig_amt.update_layout(margin_l=left_mg, margin_r=right_mg, margin_t=top_mg, margin_b=btm_mg,
-                              plot_bgcolor='white', paper_bgcolor='white',font_color="black",
+                              plot_bgcolor='white', paper_bgcolor='white', font_color="black",
                               legend=dict(bgcolor='#e7f6fa', yanchor='top', y=-0.1, xanchor='left',
-                                          x=0.015, orientation='h',font=dict( color='black')) #  orientation='h'
+                                          x=0.015, orientation='h', font=dict(color='black'))  # orientation='h'
                               )
 
-
-        fig_amt.update_xaxes(showgrid=True, gridcolor='#adb0c2',tickfont_color='black')
-        fig_amt.update_yaxes(showgrid=True, gridcolor='#adb0c2',tickfont_color='black')  # 332951
+        fig_amt.update_xaxes(showgrid=True, gridcolor='#adb0c2', tickfont_color='black')
+        fig_amt.update_yaxes(showgrid=True, gridcolor='#adb0c2', tickfont_color='black')  # 332951
         st.plotly_chart(fig_amt, use_container_width=True)
-
 
     with company_chart:
         st.markdown(
@@ -114,24 +104,29 @@ with data_space :
         pie_chart_df.columns = ['기업명', '카운트']
 
         fig_pie = pie_chart_df.iplot(kind='pie', labels='기업명', values='카운트', asFigure=True, dimensions=(400, 350),
-                       colors=('#a2b4cd','#a2a9cd',  '#77adda',  '#0eccfb', '#85d3e6','#2ebbc9'))  # '#ff4388', '#fe7e22', '#fbc120', '#4b1a84'
+                                     colors=('#a2b4cd', '#a2a9cd', '#77adda', '#0eccfb', '#85d3e6',
+                                             '#2ebbc9'))  # '#ff4388', '#fe7e22', '#fbc120', '#4b1a84'
 
         fig_pie.update_layout(margin_l=left_mg, margin_r=right_mg, margin_t=top_mg, margin_b=btm_mg,
-                              plot_bgcolor='white', paper_bgcolor='white', font_color="black",      #plot_bgcolor='#151121', paper_bgcolor='#0e1117',font_color="blue",
-                              legend=dict(bgcolor='#e7f6fa',orientation='v', font=dict(color='black'))  # orientation='h' #d9d9d9
+                              plot_bgcolor='white', paper_bgcolor='white', font_color="black",
+                              # plot_bgcolor='#151121', paper_bgcolor='#0e1117',font_color="blue",
+                              legend=dict(bgcolor='#e7f6fa', orientation='v', font=dict(color='black'))
+                              # orientation='h' #d9d9d9
 
-        )
+                              )
 
         st.plotly_chart(fig_pie, use_container_width=True)
 
     ''
     ''
     ######################### 뉴스보여주기
-    st.subheader('오늘의 이슈', help="전체 기사를 조회할 수 있는 화면입니다. 각 컬럼별 설명은 아래와 같습니다.\n- 날짜 : 각 기사가 발행된 날짜 \n- 분류 : 학습된 AI를 활용하여 카테고리를 크게 사회동향, 타사동향 및 이벤트, 상품, 제도를 구분지었습니다. \n- 기업명 : 해당 기사가 어떤 기업에 관한 내용인지 나타냅니다.\n- 제목 : 제목 선택시 해당 기사 페이지로 바로 넘어가도록 하이퍼링크가 구현되어있습니다. \n- 본문요약 : 학습된 AI가 기사 전체의 본문을 3줄 이내로 요약하여 요점을 쉽게 파악할 수 있습니다. ")
-    st.text(f'전체 수집된 기사 중 유의미 하다고 판단된 기사는 총 {df.shape[0]}건이며, 금일 기준 ({df.날짜.max()}) 수집된 주요 기사는 총 {df[df.날짜 == df.날짜.max()].shape[0]}건 입니다.')
+    st.subheader('오늘의 이슈',
+                 help="전체 기사를 조회할 수 있는 화면입니다. 각 컬럼별 설명은 아래와 같습니다.\n- 날짜 : 각 기사가 발행된 날짜 \n- 분류 : 학습된 AI를 활용하여 카테고리를 크게 사회동향, 타사동향 및 이벤트, 상품, 제도를 구분지었습니다. \n- 기업명 : 해당 기사가 어떤 기업에 관한 내용인지 나타냅니다.\n- 제목 : 제목 선택시 해당 기사 페이지로 바로 넘어가도록 하이퍼링크가 구현되어있습니다. \n- 본문요약 : 학습된 AI가 기사 전체의 본문을 3줄 이내로 요약하여 요점을 쉽게 파악할 수 있습니다. ")
+    st.text(
+        f'전체 수집된 기사 중 유의미 하다고 판단된 기사는 총 {df.shape[0]}건이며, 금일 기준 ({df.날짜.max()}) 수집된 주요 기사는 총 {df[df.날짜 == df.날짜.max()].shape[0]}건 입니다.')
     ''
-    df2 = df[['날짜','분류','기업명','제목','본문요약','url']]
-    
+    df2 = df[['날짜', '분류', '기업명', '제목', '본문요약', 'url']]
+
     title_list = []
     for idx, row in df2.iterrows():
         tmp = make_clickable([row['url'], row['제목']])
@@ -143,28 +138,27 @@ with data_space :
     df2 = df2.drop([133, 141], axis=0).reset_index(drop=True)
     ###
     df2.index = df2.index + 1
+
+
+
+    ###################
     df2 = df2[['날짜', '분류', '기업명', '제목_url', '본문요약']]
+
+    ## 라디오 추가
+    radio_sel = st.multiselect(f"수집 뉴스 조회",list(df2.분류.unique()) , default=list(df2.분류.unique()), key='part2_1',
+                                  max_selections=4)
+    tmp1 = df2[(df2['분류'].isin(radio_sel))].reset_index(drop=True)
     
-    # df2['url'] = df2['url'].apply(make_clickable)
-
-    # df2 = df2.to_html(escape=False)
-
-    df2.style.set_properties(**{'text-align': 'center'}).set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
+    ################### 가운데 정렬 설정
+    df2.style.set_properties(**{'text-align': 'center'}).set_table_styles(
+        [{'selector': 'th', 'props': [('text-align', 'center')]}])
     st.markdown('<style>.col_heading{text-align: center;}</style>', unsafe_allow_html=True)
-    df2.columns = ['<div class="col_heading">'+col+'</div>' for col in df2.columns] 
-    st.write(df2.to_html(escape=False), unsafe_allow_html=True)
+    df2.columns = ['<div class="col_heading">' + col + '</div>' for col in df2.columns]
+    st.write(tmp1.to_html(escape=False), unsafe_allow_html=True)
 
 
-    
-    # df2.style.set_properties(**{'text-align': 'center'}).set_table_styles([{'selector': 'th', 'props': [('text-align', 'center')]}])
-    # st.write(df2, unsafe_allow_html=True)
-
-    # 이것만 빼기
-    st.markdown(hide_table_row_index, unsafe_allow_html=True)
-with white_space_2 :
+with white_space_2:
     st.empty()
-
-
 
 ############################ 참고 사이트
 # 이모지 찾기 :  https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
